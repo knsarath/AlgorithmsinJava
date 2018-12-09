a0a01ffaf2459722b92f23aa9181b513b3129fb8
SARATH <sarathknonline@gmailcom>
2018-12-09 20:07:43 +0530

    merge sort

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,13 +1,12 @@
 package main;
 
 import main.algorithms.MergeSort;
-import main.algorithms.QuickSorter;
 import main.util.Print;
 
 public class Main {
     public static void main(String[] args) {
-        QuickSorter quickSorter = new QuickSorter();
-        int[] sort = quickSorter.sort(new int[]{23, 5, 2, 8, 12, 7, 16, 9, 0, 4, 8, 5});
+        MergeSort mergeSort = new MergeSort();
+        int[] sort = mergeSort.sort(new int[]{23, 5, 2, 8, 12, 7, 16, 9});
         Print.array(sort);
     }
 }
